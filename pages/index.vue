8be651c6c8208d45d5433a6904cc89c393d75f82
ivobug <ivo.bagaric.2@gmail.com>
2020-11-20 14:41:01 +0100

    bug fixed

--- pages/index.vue
+++ pages/index.vue
@@ -154,7 +154,7 @@ html{
         z-index: 3;
         white-space: nowrap;
         color: white;
-        display: flex;
+        overflow: hidden;
 
         &1{
           transform: perspective(10rem) rotateY(20deg) rotate(-1deg);
